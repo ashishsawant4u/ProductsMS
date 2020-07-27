e9c622abcb979380686636bf9f78cd14c00ba2f5
ashish.sawant-ext@rexel.com <c2Wr4w2H>
2020-07-28 01:19:05 +0530

    Using Cloud Sleuth

--- src/main/java/com/boot/demo/ProductsController.java
+++ src/main/java/com/boot/demo/ProductsController.java
@@ -3,6 +3,10 @@ package com.boot.demo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,23 +14,33 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ProductsController {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(ProductsController.class);
+	
+	@Bean
+	public RestTemplate template() {
+	    return new RestTemplate();
+	}
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	@RequestMapping("/getProducts")
 	public List<ProductData> getProducts()
 	{
+		LOG.info("inside getProducts...");
+		
+		
 		ProductData product1 = new ProductData();
 		product1.setCode("A00001");
 		product1.setName("Pureit Ultima Mineral RO + UV");
 		product1.setDesc("Pureit brings to you a water purifier that looks stunning and comes with an advanced RO + UV purification technology that not only gives you pure water but also adds minerals to enhance the taste of water.");
 		
-		RestTemplate restTemplate = new RestTemplate();
-		
-		
-		String stockAndPriceUrl = "http://localhost:8023/getStockAndPrice";
+		String stockAndPriceUrl = "http://localhost:8023/stockandpriceMS/getStockAndPrice";
 		
 		ResponseEntity<StockAndPriceData> resp = restTemplate.postForEntity(stockAndPriceUrl, product1.getCode(),StockAndPriceData.class);
 		
-		System.out.println("resp from StockAndPrice MS >> "+resp.getBody().getOfflineStock());
+		LOG.info("resp from StockAndPrice MS >> "+resp.getBody().getOfflineStock());
 		
 		product1.setStockAndPrice(resp.getBody());
 		
